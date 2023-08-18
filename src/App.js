845dfd4415a2c1d6c77fa1eb44e4e76206064ff7
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-08-18 00:58:10 -0300

    Add Header Component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ function App() {
   return (
     <div className="App">
       <Header/>
+
+      <section id='Home' className='w-full h-screen'>
+        <h3>Hi, My name is</h3>
+        <h1>ByakkoLord</h1>
+        <h2>Here u turn imagination into reality</h2>
+        <p>I am a Front-End Developer on the rise, here you will find some of my habilities, projects and ambitions.</p>
+      </section>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 function Header() {
     return (
-        <section className='flex justify-between w-full h-24 items-center'>
-            <div className="flex w-full"><img src='https://via.placeholder.com/70' alt='Logo'/></div>
+        <section className='flex fixed bg-transparent justify-between w-full h-24 items-center'>
+            <div className="flex w-full"><i id="logo_icon" class="fa-solid fa-power-off primaryColor text-5xl ml-12"/></div>
             <nav className="flex w-full justify-end">
                 <ul className="flex justify-between primaryColor text-2xl font-extrabold">
                     <li className="pr-6">Home</li>
