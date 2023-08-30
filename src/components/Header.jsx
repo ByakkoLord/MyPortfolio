07b84678fd91202f8b83da5294ecfddbe6640820
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-08-30 15:50:12 -0300

    Add Smooth Scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ function Header() {
             <nav className="flex w-full justify-end">
                 <ul className="flex justify-between primaryColor text-2xl font-extrabold">
                     <li onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})} className="hover:text-white cursor-pointer pr-6">Home</li>
-                    <a href="#Tecnologies"><li className="hover:text-white cursor-pointer pr-6">Tech</li></a>
-                    <a href="#Projects"><li className="hover:text-white cursor-pointer pr-6">Projects</li></a>
-                    <a href="#About"><li className="hover:text-white cursor-pointer pr-6">About</li></a>
-                    <li className="hover:text-white cursor-pointer pr-6">Contact</li>
+                    <a href="#Tecnologies"><li className="transition-all hover:text-white cursor-pointer pr-6">Tech</li></a>
+                    <a href="#Projects"><li className="transition-all hover:text-white cursor-pointer pr-6">Projects</li></a>
+                    <a href="#About"><li className="transition-all hover:text-white cursor-pointer pr-6">About</li></a>
+                    <li className="transition-all hover:text-white cursor-pointer pr-6">Contact</li>
                 </ul>
             </nav>
         </section>
