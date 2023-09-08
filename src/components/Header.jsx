c163760456e0017c011b0e7ca4b0fd0033853646
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-08 16:36:56 -0300

    Finished Hamburguer Button Integrations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,28 +3,16 @@ import { useState, useEffect, useRef } from 'react'
 
 function Header() {
     const [show, setShow] = useState(false)
-    const headerRef = useRef(null)
+    
 
-    useEffect(() => {
-        function handleClickOutside(event) {
-            if (headerRef.current && !headerRef.current.contains(event.target)) {
-                setShow(false)
-            }
-        }
-
-        if (show === true) {
-            document.addEventListener('click', handleClickOutside)
-        }
-        
-    }, [show])
-
+    
     
     return (
         <section className='flex fixed bg-transparent justify-between w-full h-24 items-center'>
             <div className="flex w-full"><i id="logo_icon" className="hover:cursor-pointer hover:scale-110 transition-all fa-solid fa-power-off primaryColor text-5xl ml-12"/></div>
             <nav className="flex w-full justify-end">
-                <i onClick={() => {setShow(!show)}} className="md:hidden
-                text-3xl transition-all mr-10 hover:bg-slate-600 rounded-full flex items-center justify-center w-12 h-12 primaryColor fa-solid fa-bars"></i>
+                <i onClick={() => {setShow(!show)}} className={`md:hidden
+                ${show ? style.headerButtonAnimation : style.headerButtonAnimation2} text-3xl transition-all mr-10 rounded-full flex items-center justify-center w-12 h-12 primaryColor fa-solid fa-bars`}></i>
                 <ul className={`md:static md:flex-row md:w-96 md:h-auto md:top-auto md:bg-transparent md:right-auto
                   ${show ? style.headerAnimation : style.headerAnimation2} absolute flex flex-col justify-evenly items-center w-36 primaryColor font-bold text-2xl top-0 -right-36 bg-slate-700 h-56`}>
                     <li onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})} className="hover:text-white cursor-pointer">Home</li>
