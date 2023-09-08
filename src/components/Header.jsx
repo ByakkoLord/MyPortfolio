69a1854051526eca0d19c296a502cba2750078f3
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-08 15:46:03 -0300

    Add Header Animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,36 @@
-function Header() {
-    
+import style from './Header.module.css'
+import { useState, useEffect, useRef } from 'react'
 
+function Header() {
+    const [show, setShow] = useState(false)
+    const headerRef = useRef(null)
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (headerRef.current && !headerRef.current.contains(event.target)) {
+                setShow(false)
+            }
+        }
+
+        if (show === true) {
+            document.addEventListener('click', handleClickOutside)
+        }
+        
+    }, [show])
+
+    
     return (
         <section className='flex fixed bg-transparent justify-between w-full h-24 items-center'>
             <div className="flex w-full"><i id="logo_icon" className="hover:cursor-pointer hover:scale-110 transition-all fa-solid fa-power-off primaryColor text-5xl ml-12"/></div>
             <nav className="flex w-full justify-end">
-                <ul className="lg: flex justify-between primaryColor text-2xl font-extrabold">
-                    <li onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})} className="hover:text-white cursor-pointer pr-6">Home</li>
-                    <a href="#Tecnologies"><li className="transition-all hover:text-white cursor-pointer pr-6">Tech</li></a>
-                    <a href="#Projects"><li className="transition-all hover:text-white cursor-pointer pr-6">Projects</li></a>
-                    <a href="#About"><li className="transition-all hover:text-white cursor-pointer pr-6">About</li></a>
+                <i onClick={() => {setShow(!show)}} className="md:hidden
+                text-3xl transition-all mr-10 hover:bg-slate-600 rounded-full flex items-center justify-center w-12 h-12 primaryColor fa-solid fa-bars"></i>
+                <ul className={`md:static md:flex-row md:w-96 md:h-auto md:top-auto md:bg-transparent md:right-auto
+                  ${show ? style.headerAnimation : style.headerAnimation2} absolute flex flex-col justify-evenly items-center w-36 primaryColor font-bold text-2xl top-0 -right-36 bg-slate-700 h-56`}>
+                    <li onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})} className="hover:text-white cursor-pointer">Home</li>
+                    <a href="#Tecnologies"><li className="transition-all hover:text-white cursor-pointer">Tech</li></a>
+                    <a href="#Projects"><li className="transition-all hover:text-white cursor-pointer">Projects</li></a>
+                    <a href="#About"><li className="transition-all hover:text-white cursor-pointer">About</li></a>
                 </ul>
             </nav>
         </section>
@@ -17,3 +38,5 @@ function Header() {
 }
 
 export default Header
+
+//flex flex-col justify-between primaryColor text-2xl font-extrabold
